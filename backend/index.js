25a24181a33bee97ff700f124d4fdf034c8ed76f
Chinmay M <chinmaym6166@gmail.com>
2025-06-18 17:47:29 +0530

    build

--- backend/index.js
+++ backend/index.js
@@ -529,5 +529,5 @@ app.get('/api/files', requireAuth, async (req, res) => {
 
 
 // ---- Start Server ----
-const PORT = process.env.PORT || 5000;
+const PORT = process.env.PORT;
 app.listen(PORT, () => console.log(`Server running on port ${PORT}`));
